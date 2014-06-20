679e33a1484594a8ac79f7b9eb13848fcb17e01f
azhar759 <azh4r137@gmail.com>
2014-06-20 09:27:54 +0400

    re-arranged lines of codes
    arranged lines of codes in budget and removed comment in core

--- personalbudgeting/BLL/Core.cs
+++ personalbudgeting/BLL/Core.cs
@@ -247,11 +247,3 @@ namespace PersonalBudgeting.BLL
     }
 }
 // todo: subtract safety margin along with expenditure
-
-/*
-0 – $18,200 --- Nil
-$18,201 – $37,000 --- 19c for each $1 over $18,200
-$37,001 – $80,000 --- $3,572 plus 32.5c for each $1 over $37,000
-$80,001 – $180,000 --- $17,547 plus 37c for each $1 over $80,000
-$180,001 and over --- $54,547 plus 45c for each $1 over $180,000
-*/
