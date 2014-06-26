3c349645a298cf1bd8d611d69a9c0a926f50854f
vayneesungeelee <vay.sungeelee@gmail.com>
2014-06-26 16:38:54 +0400

    cancel wallet table item test

--- personalbudgeting/BLL/Core.cs
+++ personalbudgeting/BLL/Core.cs
@@ -114,12 +114,7 @@ namespace PersonalBudgeting.BLL
             return (getAmountAvailableForGoalsPerPay(_taxRate, _superannuationRate, _listOfExpenditure, _listofIncome, noOfPayPerYear) - amountForMainGoalPerPay);
         }
 
-        public void addSavingsForMainGoal(Budget budget,double AmountToSave)
-        {
-            budget.mainGoal.AmountSaved += AmountToSave;
-            addSavingsForMainGoal(budget, AmountToSave);
-        }
-        public Boolean saveForMainGoal(BankAccount myAccount , double amountForMainGoalPerPay, MainGoal mainGoal, float _taxRate, float _superannuationRate, List<Expenditure> _listOfExpenditure, List<Income> _listofIncome, int noOfPayPerYear)
+        public Boolean saveForMainGoal(BankAccount mySavingsAccount , double amountForMainGoalPerPay, MainGoal mainGoal, float _taxRate, float _superannuationRate, List<Expenditure> _listOfExpenditure, List<Income> _listofIncome, int noOfPayPerYear)
         {
             if (getAmountAvailableForGoalsPerPay(_taxRate, _superannuationRate, _listOfExpenditure, _listofIncome, noOfPayPerYear) < amountForMainGoalPerPay)
             {
@@ -128,7 +123,7 @@ namespace PersonalBudgeting.BLL
             else
             {
                 mainGoal.AmountSaved += amountForMainGoalPerPay;
-                addToSavingsForGoals(myAccount, amountForMainGoalPerPay);
+                addToSavingsForGoals(mySavingsAccount, amountForMainGoalPerPay);
                 return true;
             }
 
@@ -176,19 +171,10 @@ namespace PersonalBudgeting.BLL
             budget.removeWalletTableItem(wti);
         }
 
-        public Boolean tickOffWalletTableItem(Budget budget, WalletTableItem wti)
+        public void tickOffWalletTableItem(Budget budget, WalletTableItem wti)
         {
-            if(wti.AmountSaved<wti.Cost)
-            {
-                return false;
-            }
-            else 
-            {
-                budget.SavingsAccount.SavingsForGoals -= wti.AmountSaved;
-                budget.removeWalletTableItem(wti);
-                return true;
-            }
-
+            budget.SavingsAccount.SavingsForGoals -= wti.AmountSaved;
+            budget.removeWalletTableItem(wti);
         }
 
         public double tickAllWalletTableItems(BankAccount myAccount,List<WalletTableItem> walletTableItems, double amountForMainGoalPerPay, float _taxRate, float _superannuationRate, List<Expenditure> _listOfExpenditure, List<Income> _listofIncome, int noOfPayPerYear)
@@ -328,11 +314,7 @@ namespace PersonalBudgeting.BLL
             }
             
         }
-        public void addIncomeForCasualWorker(Budget budget, string name, Participant source, double amount)
-        {
-           Income casualIncome=new Income(name, source, amount);
-            addToSavingsForPersonalUse(budget.SavingsAccount, casualIncome.Amount);
-        }
+
 
         public void updateBankAccount(BankAccount myAccount, float _taxRate, float _superannuationRate, List<Expenditure> _listOfExpenditure, List<Income> _listofIncome, int noOfPayPerYear, MainGoal mainGoal, double amountForMainGoalPerPay, List<WalletTableItem> _listOfWalletTableItems)
         {
@@ -356,7 +338,6 @@ namespace PersonalBudgeting.BLL
                                                         _listOfExpenditure,
                                                         _listofIncome,
                                                         noOfPayPerYear);
-
             double totalAmountTicked = tickAllWalletTableItems(myAccount,_listOfWalletTableItems, amountForMainGoalPerPay, _taxRate, _superannuationRate, _listOfExpenditure, _listofIncome, noOfPayPerYear);
             double AmountToAddToSavingsForPersonalUse;
             if (savedforMainGoal)
@@ -385,7 +366,9 @@ namespace PersonalBudgeting.BLL
             }
 
             addToSavingsForPersonalUse(myAccount, AmountToAddToSavingsForPersonalUse);
-        }       
+        }
+
+
     }
 }
 // todo: subtract safety margin along with expenditure
